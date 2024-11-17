0aa04d943fc2d00a1c744ab17f0b30aab75d00c4
atharva00-7 <atharvatandale0@gmail.com>
2024-11-17 12:54:34 +0530

    Adding wildcard route for handling incorrect paths
    Signed-off-by: atharva00-7 <atharvatandale0@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import LoginForm from "./authPages/LoginForm";
 import InterviewForm from "./pages/components/InterviewForm";
 import InterviewPage from "./pages/InterviewPage";
+import Wildcard from "./pages/Wildcard";
 
 const App = () => {
   return(
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/login" element={<LoginForm />} />
       <Route path="/interviewForm" element={<InterviewForm />} />
       <Route path="/interviewPage" element={<InterviewPage/>}/>
+      <Route path="*" element={<Wildcard />} />
     </Routes>
   )
 }
