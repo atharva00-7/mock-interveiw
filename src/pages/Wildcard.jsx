342da10247f6df30871b7dd83b5cb040b7e766ab
atharva00-7 <atharvatandale0@gmail.com>
2024-11-17 12:53:04 +0530

    Adding Wildcard error handling
    Signed-off-by: atharva00-7 <atharvatandale0@gmail.com>

---
+++ src/pages/Wildcard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Wildcard = () => {
+  return (
+    <div className='w-full flex justify-center items-center'>
+      <h1 className='text-5xl font-bold'>
+        The Route you are trying to access doesn't exist
+      </h1>
+    </div>
+  )
+}
+
+export default Wildcard
